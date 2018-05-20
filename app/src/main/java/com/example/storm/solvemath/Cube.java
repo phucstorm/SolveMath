46e8a85c139598bf922382400a35892df0eb4c12
phucstorm <t163806@vanlanguni.vn>
2018-05-20 19:36:55 +0700

    finish cude and linear

--- app/src/main/java/com/example/storm/solvemath/Cube.java
+++ app/src/main/java/com/example/storm/solvemath/Cube.java
@@ -6,14 +6,38 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Cube extends AppCompatActivity {
-
+    EditText numa;
+    Button btStep,btResult;
+    TextView step,result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cube);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        numa = (EditText) findViewById(R.id.edA);
+        step = (TextView) findViewById(R.id.tvStep);
+        result = (TextView) findViewById(R.id.tvResult);
+        btStep = (Button) findViewById(R.id.btnStep);
+        btResult = (Button) findViewById(R.id.btnResult);
+        btStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(numa.getText().toString());
+                step.setText("Ta có a = " +a+ "\n Thay a vào công thức : "+"\n => "+a+" * "+a+" * "+a+" = x");
+            }
+        });
+        btResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(numa.getText().toString());
+                result.setText("x = " + a*a*a);
+            }
+        });
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,3 +51,4 @@ public class Cube extends AppCompatActivity {
     }
 
 }
+
--- app/src/main/java/com/example/storm/solvemath/LinearEquation.java
+++ app/src/main/java/com/example/storm/solvemath/LinearEquation.java
@@ -6,14 +6,42 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LinearEquation extends AppCompatActivity {
-
+    EditText numa,numb;
+    TextView step,result;
+    Button btStep,btResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_equation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        numa = (EditText) findViewById(R.id.edA);
+        numb = (EditText) findViewById(R.id.edB);
+        step = (TextView) findViewById(R.id.tvStep);
+        result = (TextView) findViewById(R.id.tvResult);
+        btStep = (Button) findViewById(R.id.btnStep);
+        btResult = (Button) findViewById(R.id.btnResult);
+
+        btStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(numa.getText().toString());
+                int b = Integer.parseInt(numb.getText().toString());
+                step.setText("Step 1: \n " +"ax = -b <=> " + a +"x = -" + b + "\n Step 2: \n x = -b/a");
+            }
+        });
+        btResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(numa.getText().toString());
+                int b = Integer.parseInt(numb.getText().toString());
+                result.setText("x = " + (double)-b/a);
+            }
+        });
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
