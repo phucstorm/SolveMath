841a91429a819108bcc6270d88d08c0632ee695b
phucstorm <t163806@vanlanguni.vn>
2018-05-23 15:05:36 +0700

    finish Square

--- app/src/main/java/com/example/storm/solvemath/Square.java
+++ app/src/main/java/com/example/storm/solvemath/Square.java
@@ -6,14 +6,38 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Square extends AppCompatActivity {
-
+    EditText numa;
+    Button btStep,btResult;
+    TextView step,result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_square);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        numa = (EditText) findViewById(R.id.edA);
+        step = (TextView) findViewById(R.id.tvStep);
+        result = (TextView) findViewById(R.id.tvResult);
+        btStep = (Button) findViewById(R.id.btnStep);
+        btResult = (Button) findViewById(R.id.btnResult);
+        btStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(numa.getText().toString());
+                step.setText("Ta có a = " +a+ "\n Thay a vào công thức : "+"\n => "+a+"*"+a+"= x");
+            }
+        });
+        btResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(numa.getText().toString());
+                result.setText("x =" + a*a);
+            }
+        });
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
